c26e7a02612df86d2b354b6aa1a1589e20f981f9
Meggie Ladlow <meggie@unitedincome.com>
2018-10-29 15:55:33 -0400

    This won't work

---
+++ destroyer-server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/auth')
+def add_to_slack_button():
+    return app.send_static_file('add_to_slack.html')
+
+
+@app.route('/auth/redirect')
+def handle_auth_redirect():
+    options = {
+        'code': blah,
+        'client_id': os.environ('SLACK_CLIENT_ID'),
+        'client_secret': os.environ('SLACK_CLIENT_SECRET'),
+        'redirect_uri': os.environ('REDIRECT_URI')
+    }
+    return requests.get('https://slack.com/api/oauth.access', params=options)
